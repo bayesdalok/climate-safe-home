b1012084cc2edb1b9b0a261397ae8bae38a08bc8
bayesdalok <alokchandra0514@gmail.com>
2025-08-06 22:52:48 +0530

    Fix: api malfunction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from app import app  # This should match your __init__.py structure
+import os
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
